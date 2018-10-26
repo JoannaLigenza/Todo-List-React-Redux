0f8f3f155655a47974db2a683574a825db1dc42e
JoannaLigenza <ligenzajoanna@gmail.com>
2018-10-26 19:37:15 +0200

    time and priority option added

--- src/Add-task.js
+++ src/Add-task.js
@@ -5,7 +5,7 @@ class AddTask extends Component {
     state = { task: "Add new task", id: 5, list: "", date: "", time: "", priority: "", color: "" }
     handleChange = (e) => {
         this.setState( {task: e.target.value} );
-    }
+    };
     handleClick = (e) => {
         e.preventDefault();
         if(this.state.task.length === 0 ) {
@@ -13,7 +13,7 @@ class AddTask extends Component {
         }
         this.addTask();
         this.props.hideAddNewTask();
-    }
+    };
     addTask = (e) => {
         // console.log("tuuu " ,this.props.tasks)
         // console.log(this.state)
@@ -22,13 +22,23 @@ class AddTask extends Component {
         this.props.addTask(this.state.task, this.state.id, this.state.list, this.state.date, this.state.time, this.state.priority, this.state.color);
         this.setState( {task: ""})
         this.setState( {date: ""});
-    }
+    };
     selectChange = (e) => {
         this.setState( {list: e.target.value} );
-    }
+    };
+    
     dateChange = (e) => {
         this.setState( {date: e.target.value} );
-    }
+    };
+
+    timeChange = (e) => {
+        this.setState( {time: e.target.value} );
+    };
+
+    priorityChange = (e) => {
+        console.log("e.target.value ", e.target.value)
+        this.setState( {priority: e.target.value} );
+    };
     
     render() {
         const lists = this.props.lists.map( list => {
@@ -51,8 +61,30 @@ class AddTask extends Component {
                         <h4>Date:</h4>
                         <input type="date" value={this.state.date} onChange={this.dateChange}></input>
                     </div>
-                    <div className="add-priority priority-time"></div>
-                    <div className="add-priority priority-priority"></div>
+                    <div className="add-priority priority-time">
+                        <h4>Time:</h4>
+                        <input type="time" value={this.state.time} onChange={this.timeChange}></input>
+                    </div>
+                    {/* <div className="add-priority priority-priority">
+                        <h4>Priority:</h4>
+                        <select label="wybierz" onChange={this.priorityChange} default="none">
+                            <option>None</option>
+                            <option>Low</option>
+                            <option>Middle</option>
+                            <option>High</option>
+                        </select>
+                    </div> */}
+                    <div className="add-priority priority-priority">
+                        <h4>Priority:</h4>
+                        <div id="priority-select" onChange={this.priorityChange} default="none">
+                            <ul>
+                                <li><label for="1"><input type="radio" id="1"name="prior" value="None"></input>None</label></li>
+                                <li><label for="2"><input type="radio" id="2"name="prior" value="Low"></input>Low</label></li>
+                                <li><label for="3"><input type="radio" id="3"name="prior" value="Middle"></input>Middle</label></li>
+                                <li><label for="4"><input type="radio" id="4"name="prior" value="High"></input>High</label></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 <button className="button-to-input" onClick={this.handleClick}>Add Task</button>
             </div>
