36aa450a5aeb469b8a3f3e2c5380f871ffbb1b2b
JoannaLigenza <ligenzajoanna@gmail.com>
2019-10-10 18:13:39 +0200

    editing notes removed

---
+++ src/NoteList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const NoteList = ( {notes, deleteNote, editNote} ) => {
+    const showNotes = notes.map( (note) => {
+        return <li className="one-note" key={note.id}>
+            <p className="note-text" >{note.note}</p>
+            <div className="delete-note-button" onClick={ () => {deleteNote(note.id)} }></div>
+            </li>
+    })
+    return(
+        <div id="notes">
+            <ul id="note-list">
+                {showNotes}
+            </ul>
+        </div>
+    )
+
+}
+
+const mapStateToProps = (state) => {            // state is form redux store (from imported connect)
+    return {
+        notes: state.notes
+    }
+}
+
+const mapDispatchToPost = (dispatch) => {
+    return {
+        deleteNote: (id) => { dispatch( { type: 'DELETE_NOTE', id: id} ) }, 
+        editNote: (text, id) => { dispatch( { type: 'EDIT_NOTE', note: text, id: id} ) }, 
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToPost)(NoteList);
